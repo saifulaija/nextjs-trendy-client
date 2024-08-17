7290ecb671e6ae32fee4bc87c5929410e81f1266
saifulaija <saifulaija2023@gmail.com>
2024-08-18 01:30:05 +0600

    initial setup done

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>this is home page</p>
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="border-gray-300 h-12 w-12 animate-spin rounded-full border-4 border-t-primary"></div>
+    </div>
+  );
+};
+
+export default Loader;
